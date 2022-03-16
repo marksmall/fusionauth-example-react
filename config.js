f070b8935e56d6dded2962d9ff5da61b0585ce83
Mark Small <mark.small@astrosat.space>
2022-03-16 10:53:18 +0000

    fix(devops): Update config to match my local fusionauth setup
    Following the blog:
    
    https://fusionauth.io/blog/2020/03/10/securely-implement-oauth-in-react
    
    I created a new app/user etc in fusionauth. These new values
    in this file are the generated values from that work.

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 module.exports = {
+  // FusionAuth info (copied from the FusionAuth admin panel)
+  clientID: "53d473e5-9509-4099-9be1-26f7a8f1cceb",
+  clientSecret: "ALG7Uig0LHmee4SRKabf3ZWpD2D1xBoY097t1_5DI3U",
+  redirectURI: "http://localhost:9000/oauth-callback",
+  applicationID: "53d473e5-9509-4099-9be1-26f7a8f1cceb",
 
-	// FusionAuth info (copied from the FusionAuth admin panel)
-	clientID: '85a03867-dccf-4882-adde-1a79aeec50df',
-	clientSecret: 'JNlTw3c9B5NrVhF-cz3m0fp_YiBg-70hcDoiQ2Ot30I',
-	redirectURI: 'http://localhost:9000/oauth-callback',
-	applicationID: '85a03867-dccf-4882-adde-1a79aeec50df',
+  // our FusionAuth api key
+  apiKey: "CIiFuvEu3T4AJMwzHy4dQiwDkzEDLzQ-cp-CK_S35kek5QjZDO-l7yaF",
 
-	// our FusionAuth api key
-	apiKey: 'bf69486b-4733-4470-a592-f1bfce7af580',
-
-	// ports
-	clientPort: 8080,
-	serverPort: 9000,
-	fusionAuthPort: 9011
+  // ports
+  clientPort: 8080,
+  serverPort: 9000,
+  fusionAuthPort: 9011,
 };
